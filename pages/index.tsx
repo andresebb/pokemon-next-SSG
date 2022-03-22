749ff176992d6ca476522b7e903bbc889b6f75c8
Andres Betancourt <aebb001@gmail.com>
2022-03-21 22:50:36 -0500

    Getting data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,42 @@
-import type { NextPage } from 'next'
-import { Button } from '@nextui-org/react';
+import type { GetStaticProps, NextPage } from 'next'
 import { Layout } from '../components/layout';
+import { pokeApi } from '../api';
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
+
+  console.log({ props })
+
   return (
     <Layout title="Mi app pokemona">
-      <Button color="gradient">Hola mundo</Button>
+      <ul>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+        <li>Pokemon</li>
+      </ul>
     </Layout>
   )
+}
+
+// GetStaticProps solo se pueden usar en Pages
+// Solo usar cuando estes seguro que el contenido sera static
+// Esta funcion solo se ejecuta del lado del servidor,
+// Solo se ejecuta en build time
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+  const { data } = await pokeApi.get("/pokemon?/limit=151")
+
+
+  return {
+    props: {
+      pokemons: data.results
+    }
+  }
 }
 
 export default Home
